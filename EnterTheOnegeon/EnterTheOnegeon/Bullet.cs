da673cab9ffa466791519546b9d35f074df6bf3a
bmk4702 <bmk4702@rit.edu>
2022-02-16 11:45:12 -0500

    Bullet Class

--- EnterTheOnegeon/EnterTheOnegeon/Bullet.cs
+++ EnterTheOnegeon/EnterTheOnegeon/Bullet.cs
@@ -6,11 +6,41 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 namespace EnterTheOnegeon
 {
+    // Bullet Class
+    // Child of GameObject
     abstract class Bullet : GameObject 
     {
-        public Bullet(Texture2D sprite, Rectangle hitbox) :base(sprite, hitbox)
+        // Field
+        private int speed;
+
+        // Property
+        public int Speed
+        { get { return speed; } }
+
+        // Constructor
+        // Parameterized
+        public Bullet(int speed, Texture2D sprite, Rectangle hitbox) : base(sprite, hitbox)
+        {
+            this.speed = speed;
+        }
+
+        public override void Update()
         {
 
+        }
+
+        public override void Move()
+        {
+
+        }
+
+        // Draw Bullet
+        public override void Draw(SpriteBatch sb)
+        {
+            sb.Draw(
+                sprite,
+                hitbox,
+                Color.White);
         }
     }
 }
