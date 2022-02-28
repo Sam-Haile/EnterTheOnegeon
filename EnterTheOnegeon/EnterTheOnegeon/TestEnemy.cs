0731665cc5e1da073c03f9f9cbae0acf39b329aa
Nelson Lei <nzl4096@rit.edu>
2022-02-28 11:13:58 -0500

    not returning the normal vector anymore for vectortoposition

--- EnterTheOnegeon/EnterTheOnegeon/TestEnemy.cs
+++ EnterTheOnegeon/EnterTheOnegeon/TestEnemy.cs
@@ -10,9 +10,13 @@ namespace EnterTheOnegeon
     class TestEnemy : Enemy
     {
         int speed;
+        Vector2 place;
+        Random rand;
         public TestEnemy(Texture2D sprite, Rectangle rectangle, int health) : base(sprite, rectangle, health)
         {
             speed = 5;
+            rand = new Random();
+            place = new Vector2(rand.Next(0, 600), rand.Next(0, 450));
         }
         public override bool CollideWith(GameObject other)
         {
@@ -27,11 +31,16 @@ namespace EnterTheOnegeon
         }
 
         /// <summary>
-        /// Moves arbitrarily to a point of the screen for now
+        /// Moves to random point
         /// </summary>
         public override void Move()
         {
-            Vector2 direction = this.VectorToPosition(300, 300);
+            Vector2 direction = this.VectorToPosition(place);
+            if(rectangle.X < 0 || rectangle.Y < 0 || rectangle.X > 600 || rectangle.Y > 450)
+            {
+                place.X = rand.Next(0, 600);
+                place.Y = rand.Next(0, 450);
+            }
             rectangle.X += (int) (direction.X * speed);
             rectangle.Y += (int) (direction.Y * speed);
         }
