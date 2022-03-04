c170f3e5e29ff3d7c4ab98bc59becac7b2be6d0e
Nelson Lei <nzl4096@rit.edu>
2022-03-04 17:40:00 -0500

    PositionV for getting postion vector

--- EnterTheOnegeon/EnterTheOnegeon/Sprites/TestEnemy.cs
+++ EnterTheOnegeon/EnterTheOnegeon/Sprites/TestEnemy.cs
@@ -10,13 +10,13 @@ namespace EnterTheOnegeon
     class TestEnemy : Enemy
     {
         int speed;
-        Vector2 place;
+        Vector2 playerPos;
         Random rand;
         public TestEnemy(Texture2D sprite, Rectangle rectangle, int health) : base(sprite, rectangle, health)
         {
             speed = 5;
             rand = new Random();
-            place = new Vector2(rand.Next(0, 600), rand.Next(0, 450));
+            playerPos = new Vector2(rand.Next(0, 600), rand.Next(0, 450));
         }
         public override bool CollideWith(GameObject other)
         {
@@ -35,18 +35,8 @@ namespace EnterTheOnegeon
         /// </summary>
         public override void Move()
         {
-            Vector2 direction = this.VectorToPosition(place);
+            Vector2 direction = this.VectorToPosition(playerPos);
             
-            if(rectangle.X < 0 || rectangle.Y < 0 || rectangle.X > 600 || rectangle.Y > 450)
-            {
-                place.X = rand.Next(0, 600);
-                place.Y = rand.Next(0, 450);
-            }
-            else if(direction.X < 5 || direction.Y < 5)
-            {
-                place.X = rand.Next(0, 600);
-                place.Y = rand.Next(0, 450);
-            }
             direction.Normalize();
             rectangle.X += (int) (direction.X * speed);
             rectangle.Y += (int) (direction.Y * speed);
@@ -56,5 +46,11 @@ namespace EnterTheOnegeon
         {
             this.Move();
         }
+        //
+        public void Relocate(Player p)
+        {
+            playerPos = p.PositionV;
+        }
+
     }
 }
