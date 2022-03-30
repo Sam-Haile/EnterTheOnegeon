8b7ca25440b6ff4a2f17180c8aa514d7b02d420c
Nelson Lei <nzl4096@rit.edu>
2022-03-30 11:13:32 -0400

    bulletstats

---
+++ EnterTheOnegeon/EnterTheOnegeon/Sprites/BulletStats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace EnterTheOnegeon.Sprites
+{
+    public struct BulletStats
+    {
+        public BulletStats(int speed, int passes)//int damage, int lifetime
+        {
+            
+        }
+        public int Speed 
+        {
+            get { return 0; }
+        }
+    }
+}
